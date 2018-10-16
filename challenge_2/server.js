e696fd2253d7d6c6cc688dfb189e999483cb231d
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-10-16 16:59:25 -0700

    add form capability to receive post requests

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 var http = require('http');
+var bodyParser = require('body-parser');
 
 app.use(express.static('client'));
+// app.use(bodyParser);
 // app.get('/', (request, response) => {
   // if (error)
 // });
 
-app.post('/', (request, response) =>{
-  console.log('this is a POST request');
+app.post('/', (request, response) => {
+  // JSON stringify the request, and then convert it to CSV
+  // send the CSV as a response
+  console.log('this is a POST request', request);
 })
 
 console.log('listening on port 3000');
