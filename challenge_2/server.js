73444fe96b37967dd92bb990001df8e56090c366
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-10-16 15:20:20 -0700

    add server spinning up html

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+
+app.use(express.static('client'));
+// app.get('/', (request, response) => {
+  // if (error)
+// });
+
+app.post('/', (request, response) =>{
+  console.log('this is a POST request');
+})
+
+console.log('listening on port 3000');
+app.listen(3000);
 
 
