7a28377906caef98d5f0ac0365690e4e3f6d2038
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-10-16 17:29:29 -0700

    add correct values showing up in POST requests

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -4,7 +4,8 @@ var http = require('http');
 var bodyParser = require('body-parser');
 
 app.use(express.static('client'));
-// app.use(bodyParser);
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // app.get('/', (request, response) => {
   // if (error)
 // });
@@ -12,7 +13,8 @@ app.use(express.static('client'));
 app.post('/', (request, response) => {
   // JSON stringify the request, and then convert it to CSV
   // send the CSV as a response
-  console.log('this is a POST request', request);
+  console.log('this is a POST request', request.body.name);
+  // response.end();
 })
 
 console.log('listening on port 3000');
