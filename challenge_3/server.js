80b171b007953a04b155960eba751674d8c01e5e
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-10-17 11:58:58 -0700

    added dependencies and server base

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+const port = 3000;
+
+app.use(express.static('client'));
 
 
+
+app.listen(port, console.log('listening on port ', port));
