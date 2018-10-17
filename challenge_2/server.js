6a7fe40fb90154ff32da119ab98676c63c2b604e
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-10-16 23:28:05 -0700

    add appendfile technique, need to refactor

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,20 +2,29 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var bodyParser = require('body-parser');
-
+var fs = require('fs');
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // app.get('/', (request, response) => {
   // if (error)
 // });
 
 app.post('/', (request, response) => {
-  // JSON stringify the request, and then convert it to CSV
   // send the CSV as a response
+    // take the request and then 
+    fs.appendFile('./client/index.html', request.body.name, (err, data) =>{
+      if (err) {
+        response.statusCode(500).send()
+      } else {
+        console.log('appended succesfully');
+      }
+    });
   console.log('this is a POST request', request.body.name);
   // response.end();
-})
+  // console.log('end', response.end)
+});
 
 console.log('listening on port 3000');
 app.listen(3000);
